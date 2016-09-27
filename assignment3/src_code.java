18548d9d367d86c4a880992b32d9c73f4d08b77d
Yang_LI_NEU2016 <li.yang1@husky.neu.edu>
2016-09-26 21:29:22 -0700

    Update src_code.java

--- assignment3/src_code.java
+++ assignment3/src_code.java
@@ -11,6 +11,9 @@ public class Assignment3 {
 	 * return {1,3,9,27}
 	 */
 	public int[] findPowerOfThree(int n) {
+		if (n < 1) {
+			return null;
+		}
 		int number = 3; 
 		int arr[] = new int[n];
 		arr[0] = 1;
