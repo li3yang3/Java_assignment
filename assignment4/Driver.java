12468a586c26354279b540fa1e73219369c5c870
Yang_LI_NEU2016 <li.yang1@husky.neu.edu>
2016-10-05 14:48:16 -0700

    Update Driver.java

--- assignment4/Driver.java
+++ assignment4/Driver.java
@@ -1,4 +1,6 @@
-
+/*
+5. Combine with problem 3 and 4, define a class named Driver that contains methods like drive and addGas so that the driver can drive the car.
+*/
 
 public class Driver {
        String name;
