567838520666553f188a09e067f96656c76d3642
Yang_LI_NEU2016 <li.yang1@husky.neu.edu>
2016-10-05 14:47:05 -0700

    Create Driver.java

---
+++ assignment4/Driver.java
@@ -0,0 +1,30 @@
+public class Driver {
+       String name;
+       int licenseNo;
+       Double gasamount;
+       int age;
+       
+       public Driver(String name,int licenseNo,int age){
+    	   this.name = name;
+    	   this.licenseNo = licenseNo;
+    	   this.age = age;
+    	   
+       }
+       
+       public void drive(){
+    	   System.out.println("on the road");
+       }
+       
+       public void naviagte(){
+    	   System.out.println("navigating to the destination");
+       }
+       
+       public void addGas(Double addvol){
+    	   GasTank tank = new GasTank();
+    	   tank.addGas(addvol);;
+       }
+       public void getGasLevel() {
+    	   GasTank tank = new GasTank();
+    	   tank.getGasLevel();
+       }
+}
